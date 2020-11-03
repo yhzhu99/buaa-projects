8fe43e6c1ad0744f89f4c489d7a9921208748cd4
Yinghao Zhu <zhuyinghao1999@msn.cn>
2020-11-04 00:03:39 +0800

    fetch sometest data

--- OPG.c
+++ OPG.c
@@ -3,8 +3,8 @@
 FILE *in;
 char c;
 int main(int argc, char* argv[]){
-    in=fopen(argv[1],"r");
-    // in=fopen("test.in","r");
+    //in=fopen(argv[1],"r");
+    in=fopen("test.in","r");
     c=fgetc(in);
     while(c!=EOF){
         printf("%c",c);

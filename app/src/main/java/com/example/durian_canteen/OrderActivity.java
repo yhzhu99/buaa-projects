7ad0800e8446505337abedb4dc303a66f775734f
Yinghao Zhu <yhzhu99@gmail.com>
2021-11-12 00:32:14 +0800

    chore: bind elements in order page

--- app/src/main/java/com/example/durian_canteen/OrderActivity.java
+++ app/src/main/java/com/example/durian_canteen/OrderActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
 import androidx.annotation.Nullable;
 
 public class OrderActivity extends Activity {
@@ -23,6 +27,19 @@ public class OrderActivity extends Activity {
         ImageView[] views = new ImageView[105];
         views[0] = (ImageView) findViewById(R.id.dish1);
         views[1] = (ImageView) findViewById(R.id.dish2);
+        RadioButton[] radioButtons = new RadioButton[105];
+        radioButtons[0] = (RadioButton) findViewById(R.id.radio_dish1);
+        radioButtons[1] = (RadioButton) findViewById(R.id.radio_dish2);
+        TextView[] dishTexts = new TextView[105];
+        dishTexts[0] = (TextView) findViewById(R.id.text_dish1);
+        dishTexts[1] = (TextView) findViewById(R.id.text_dish2);
+        int dishMoney[] = new int[105];
+        dishMoney[0] = 88;
+        dishMoney[1] = 58;
+
+        System.out.println("----------aaa--------"+dishTexts[0].getText().toString()+dishTexts[1].getText().toString());
+        dishTexts[0].setText("牛排");
+        System.out.println("----------bbb--------"+dishTexts[0].getText().toString()+dishTexts[1].getText().toString());
         checkout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
